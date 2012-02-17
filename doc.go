ae912e63c2bd67d95088521f199731b8c2685af9
Daniel Connelly <dhconnelly@gmail.com>
2012-02-17 01:00:47 -0500

    sections rendered in table rows

--- doc.go
+++ doc.go
@@ -7,14 +7,40 @@ import (
 	"io/ioutil"
 	"litebrite"
 	"text/template"
+	"strings"
 )
 
-var match = regexp.MustCompile("^\\s*//[^\n]")
+var match = regexp.MustCompile("^\\s*//[^\n]\\s?")
 var t = template.Must(template.ParseFiles("doc.templ"))
+
+type section struct {
+	Doc string
+	Code string
+}
+
+func extractSections(source string) []section {
+	sections := make([]section, 0)
+	var current section
+	
+	for _, line := range strings.Split(source, "\n") {
+		if match.FindString(line) != "" {
+			if current.Code != "" {
+				sections = append(sections, current)
+				current = section{}
+			}
+			repl := match.ReplaceAllString(line, "")
+			current.Doc += repl + "\n"
+		} else {
+			current.Code += line + "\n"
+		}
+	}
+
+	return append(sections, current)
+}
 
 type File struct {
 	Title string
-	Source string
+	Sections []section
 }
 
 func main() {
@@ -24,8 +50,18 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 		}
-		
-		out := litebrite.Highlight(string(src))
-		t.Execute(os.Stdout, File{filename, out})
+
+		h := new(litebrite.Highlighter)
+		h.IdentClass = "ident"
+		h.LiteralClass = "literal"
+		h.KeywordClass = "keyword"
+		h.OperatorClass = "operator"
+		out := h.Highlight(string(src))
+
+		sections := extractSections(out)
+		errt := t.Execute(os.Stdout, File{filename, sections})
+		if errt != nil {
+			fmt.Fprintf(os.Stderr, errt.Error())
+		}
 	}
 }
